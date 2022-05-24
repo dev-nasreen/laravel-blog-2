3deb449cf6f37b920bb3cf875d503f770ab15865
dev-nasreen <nasreen.akter34@gmail.com>
2022-05-24 17:22:52 +0600

    user table mass assignment updated

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -2,14 +2,25 @@
 
 namespace App\Http\Controllers;
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class FrontEndController extends Controller
 {
    public function home(){
-    $recentPosts = Post::with('category')->orderBy('created_at', 'DESC')->paginate(9);
+    $posts = Post::with('category', 'user')->orderBy('created_at', 'DESC')->take(5)->get();
+    $firstPosts2 = $posts->splice(0, 2);
+    $middlePosts = $posts->splice(0, 1);
+    $lastPosts = $posts->splice(0);
+ 
+    $footerposts = Post::with('category', 'user')->inRandomOrder()->limit(4)->get();
+    $firstFooterPost = $footerposts->splice(0, 1);
+    $middleFooterPost = $footerposts->splice(0, 2);
+    $lastFooterPost = $footerposts->splice(0);
+    
+    $recentPosts = Post::with('category', 'user')->orderBy('created_at', 'DESC')->paginate(9);
   
-    return view('website.home', compact(['recentPosts']));
+    return view('website.home', compact(['recentPosts', 'firstPosts2','middlePosts','lastPosts', 'posts','firstFooterPost','middleFooterPost', 'lastFooterPost']));
    }
    public function about(){
     return view('website.about');
